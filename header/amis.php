9586a5ebe3fe87e71c64da25f9f25598e9beb15f
Etakine <mikado.travian@gmail.com>
2014-01-04 12:29:43 +0100

    Update amis.php

---
+++ header/amis.php
@@ -0,0 +1,12 @@
+<?php
+
+if(!empty($_GET['var1']) && !empty($_GET['var2']) &&
+   ($_GET['var2'] == 'supprimer')) {
+        $paquet->add_action('supprimer_amis', array($_GET['var1']));
+}
+else {
+        $paquet->add_action('get_mes_amis');
+}
+$paquet->send_action();
+
+?>

0bea932d2eed554351eac1defbd97c0fea48247e
Etakine <mikado.travian@gmail.com>
2014-01-04 12:43:15 +0100

    Update boiteenvoie.php

---
+++ header/boiteenvoie.php
@@ -0,0 +1,19 @@
+<script type="text/javascript">
+$(function(){
+        $(".supr_messagerie").click( function() {
+                $(this).parent().parent().find("td:parent").hide("slow");
+        });
+});
+
+function suppr_mp(id){
+     $.ajax({
+       type: "POST",
+       url: "form/supprimer_mp.php",
+       data: "id="+id,
+       success:
+       function(msg){
+$("#message_"+id).hide('slow');
+       }
+     });
+}
+</script>
